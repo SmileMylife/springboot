7ea61a7f23838704e7bfe3ce3563b3619fb2bbbd
SmileMylife <843833414@qq.com>
2018-12-30 17:31:54 +0800

    学习使用restTemplate使用方法

--- src/main/java/com/example/springboot/controller/TestSpringBootController.java
+++ src/main/java/com/example/springboot/controller/TestSpringBootController.java
@@ -4,11 +4,19 @@ import com.example.springboot.bean.InputObject;
 import com.example.springboot.bean.OutputObject;
 import com.example.springboot.service.ITestSpringBootService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,5 +49,53 @@ public class TestSpringBootController {
     public OutputObject testDaoOperation(@com.example.springboot.annotations.InputObject InputObject inputObject, OutputObject outputObject) {
         iTestSpringBootService.testParamsPackage(inputObject, outputObject);
         return outputObject;
+    }
+
+    /**
+     * 测试使用spring自带的类似HttpClient的工具调用http接口。
+     * @param inputObject
+     * @param outputObject
+     * @return
+     */
+    @RequestMapping(value = "/directIndex", method = RequestMethod.POST)
+    @ResponseBody
+    public String directIndex(InputObject inputObject, OutputObject outputObject) {
+        /*这种方法，发出去的参数在springmvc中接收不到
+
+        RestTemplate restTemplate = new RestTemplate();
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("xmlhead", "请求头");
+        map.put("xmlbody", "请求体");
+        map.put("provCode", "00030016");
+
+        HttpEntity<Map<String, Object>> objectHttpEntity = new HttpEntity<>(map);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Type", MediaType.MULTIPART_FORM_DATA.toString());
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity("http://127.0.0.1:8080/testRestTemplate", objectHttpEntity, String.class);
+        String body = responseEntity.getBody();
+        System.out.println("响应消息内容：" + body);*/
+
+
+        //下面这种方法在springmvc中可以接收到
+        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        map.add("xmlhead", "请求头");
+        map.add("xmlbody", "请求体");
+        map.add("provCode", "省份编码");
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://127.0.0.1:8080/testRestTemplate";
+        String s = restTemplate.postForObject(url, map, String.class);
+        System.out.println(s);
+        return s;
+    }
+
+    @RequestMapping(value = "/testRestTemplate", method = RequestMethod.POST)
+    @ResponseBody
+    public String testRestTemplate(String xmlhead, String xmlbody, String provCode, HttpServletRequest httpServletRequest) {
+        Map<String, String[]> parameterMap = httpServletRequest.getParameterMap();
+        System.out.println("报文头为：" + xmlhead);
+        System.out.println("报文体为：" + xmlbody);
+        System.out.println("省份编码为：" + provCode);
+        String responseBody = "<?xml version = '1.1.0'><library><book>三国演义</book></library>";
+        return responseBody;
     }
 }
