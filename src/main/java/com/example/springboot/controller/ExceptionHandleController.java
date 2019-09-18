7b3a82c8acf1f285fe88bc905b5d05e20875dbbc
SmileMylife <843833414@qq.com>
2019-09-18 17:47:11 +0800

    springboot异常处理机制，接收request和exception两个参数，处理完成可跳转至特定页面

--- src/main/java/com/example/springboot/controller/ExceptionHandleController.java
+++ src/main/java/com/example/springboot/controller/ExceptionHandleController.java
@@ -7,23 +7,23 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * @controllerAdvice并不会被当成controller来进行处理，而且接收参数只能是exception和request
  * Created by ZhangPei on 2018/12/11.
  */
 @ControllerAdvice(basePackages = "com.example.springboot.controller")
 public class ExceptionHandleController {
 
     /**
+     * @controllerAdvice并不会被当成controller来进行处理，而且接收参数只能是exception和request
+     * 当相应的异常处理机制处理完相应的异常之后，可返回json信息，也可以跳转至相应的视图层，返回modelAndView即可。
      * 异常处理机制，可返回视图层或者数据，当返回视图层时使用如下方法，当返回数据时使用responsebody
      * @param e
      * @return
      */
     @ExceptionHandler(value = Exception.class)
     public ModelAndView handleException(HttpServletRequest request, Exception e) {
-        System.out.println(request);
         System.out.println("程序发生异常，异常原因：" + e.toString());
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("error");
+        modelAndView.setViewName("/error/comError");
         modelAndView.addObject("errorDesc", e.toString());
         return modelAndView;
     }
